20e8d81f16be126f2267cb6040390adcf6a8f565
ShaunBillows <billows.shaun@gmail.com>
2022-07-14 11:49:09 +0100

    Card component added

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import './Card.css'
+
+const Card = (props) => {
+
+    return (
+        <div className='card-container'>
+            <div className='card-item card-image'>
+                <img src={props.url}/> 
+            </div>
+            <h2 className='card-item card-title' >{props.breed}</h2>
+            <div className='card-item card-text'>
+                <p>
+                    {props.name}
+                </p>
+                <p>
+                    £{props.price}
+                </p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Card
